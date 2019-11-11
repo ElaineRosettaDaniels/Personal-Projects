ccae2c3f23ce0805251fc6f001617ef35b1968ba
ArtificerEllie <elainedaniels457@gmail.com>
2019-11-11 17:38:22 -0500

    added two functions for die roll

--- tavernsimulator/src/main/resources/static/gamelogic.js
+++ tavernsimulator/src/main/resources/static/gamelogic.js
@@ -5,12 +5,42 @@ var currRepairs = $("#tavernRepairs").val();
 var foodToAdd = $("#foodToAdd").val();
 var drinksToAdd = $("#drinksToAdd").val();
 var repairsToAdd = $("#repairsToAdd").val();
-var gpPerFood = 3;
-var gpPerDrink = 2;
-var gpPerRepair = 4;
+var promotionGold = $("#weekPromote").val();
+var gpPerFood = 1;
+var gpPerDrink = 1;
+var gpPerRepair = 2;
+var tendayExpense = 50;
 
-funciton rollForTenday() {
-    
+function calcRollMod() {
+    var rollMod = 10;
+    if (currFood < 1) {
+        rollMod - 25;
+    }
+
+    if (currDrinks < 1) {
+        rollMod - 40;
+    }
+
+    if (currRepairs < 10) {
+        rollMod - 25;
+    } else if (currRepairs < 20) {
+        rollMod - 20;
+    } else if (currRepairs < 30) {
+        rollMod - 15;
+    } else if (currRepairs < 40) {
+        rollMod - 10;
+    } else if (currRepairs < 50) {
+        rollMod - 5;
+    }
+
+    rollMod += promotionGold;
+    return rollMod;
+}
+
+function rollForTenday() {
+    var roll = Math.floor(Math.random() * 100) + 1;
+    roll += rollMod;
+    return roll;
 }
 
 $(document).ready(function(){
