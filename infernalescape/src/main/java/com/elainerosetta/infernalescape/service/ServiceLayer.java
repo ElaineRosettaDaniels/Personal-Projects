8e975e86c2509f27da1522a8a6c586ca56d647c0
ArtificerEllie <elainedaniels457@gmail.com>
2019-11-26 17:26:54 -0500

    Added and completed passthru methods in ServiceLayerImpl

--- infernalescape/src/main/java/com/elainerosetta/infernalescape/service/ServiceLayer.java
+++ infernalescape/src/main/java/com/elainerosetta/infernalescape/service/ServiceLayer.java
@@ -5,10 +5,42 @@
  */
 package com.elainerosetta.infernalescape.service;
 
+import com.elainerosetta.infernalescape.dto.Rider;
+import com.elainerosetta.infernalescape.dto.Station;
+import com.elainerosetta.infernalescape.dto.Vehicle;
+import java.util.List;
+
 /**
  *
  * @author Asus1
  */
 public interface ServiceLayer {
+
     
+    
+    // VehicleDao passthru
+    Vehicle getVehicleById(int vehicleId);
+    List<Vehicle> getAllVehicles();
+    Vehicle addVehicle(Vehicle v);
+    void updateVehicle(Vehicle v);
+    void deleteVehicle(int vehicleId);
+    
+    // StationDao passthru
+    Station getStationById(int stationId);
+    List<Station> getAllStations();
+    List<Station> getStationsForVehicle(int vehicleId);
+    Station addStation(Station s);
+    void addStationToVehicle(Station s, Vehicle v);
+    void updateStation(Station s);
+    void deleteStation(Station s);
+    
+    // RiderDao passthru
+    Rider getRiderById(int riderId);
+    List<Rider> getAllRiders();
+    List<Rider> getRidersForVehicle(int vehicleId);
+    Rider addRider(Rider r);
+    void addRiderToVehicle(Rider r, Vehicle v);
+    void addRiderToStation(Rider r, Station s);
+    void updateRider(Rider r);
+    void deleteRider(Rider r);
 }
--- infernalescape/src/main/java/com/elainerosetta/infernalescape/service/ServiceLayerImpl.java
+++ infernalescape/src/main/java/com/elainerosetta/infernalescape/service/ServiceLayerImpl.java
@@ -5,6 +5,14 @@
  */
 package com.elainerosetta.infernalescape.service;
 
+import com.elainerosetta.infernalescape.dao.RiderDao;
+import com.elainerosetta.infernalescape.dao.StationDao;
+import com.elainerosetta.infernalescape.dao.VehicleDao;
+import com.elainerosetta.infernalescape.dto.Rider;
+import com.elainerosetta.infernalescape.dto.Station;
+import com.elainerosetta.infernalescape.dto.Vehicle;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -12,6 +20,115 @@ import org.springframework.stereotype.Repository;
  * @author Asus1
  */
 @Repository
-public class ServiceLayerImpl {
+public class ServiceLayerImpl implements ServiceLayer {
+
+    @Autowired
+    VehicleDao veDao;
+    
+    @Autowired
+    StationDao stDao;
+    
+    @Autowired
+    RiderDao riDao;
+    
+    @Override
+    public Vehicle getVehicleById(int vehicleId) {
+        return veDao.getVehicleById(vehicleId);
+    }
+
+    @Override
+    public List<Vehicle> getAllVehicles() {
+        return veDao.getAllVehicles();
+    }
+
+    @Override
+    public Vehicle addVehicle(Vehicle v) {
+        return veDao.addVehicle(v);
+    }
+
+    @Override
+    public void updateVehicle(Vehicle v) {
+        veDao.updateVehicle(v);
+    }
+
+    @Override
+    public void deleteVehicle(int vehicleId) {
+        veDao.deleteVehicle(vehicleId);
+    }
+
+    @Override
+    public Station getStationById(int stationId) {
+        return stDao.getStationById(stationId);
+    }
+
+    @Override
+    public List<Station> getAllStations() {
+        return stDao.getAllStations();
+    }
+
+    @Override
+    public List<Station> getStationsForVehicle(int vehicleId) {
+        return stDao.getStationsForVehicle(vehicleId);
+    }
+
+    @Override
+    public Station addStation(Station s) {
+        return stDao.addStation(s);
+    }
+
+    @Override
+    public void addStationToVehicle(Station s, Vehicle v) {
+        stDao.addStationToVehicle(s, v);
+    }
+
+    @Override
+    public void updateStation(Station s) {
+        stDao.updateStation(s);
+    }
+
+    @Override
+    public void deleteStation(Station s) {
+        stDao.deleteStation(s);
+    }
+
+    @Override
+    public Rider getRiderById(int riderId) {
+        return riDao.getRiderById(riderId);
+    }
+
+    @Override
+    public List<Rider> getAllRiders() {
+        return riDao.getAllRiders();
+    }
+
+    @Override
+    public List<Rider> getRidersForVehicle(int vehicleId) {
+        return riDao.getRidersForVehicle(vehicleId);
+    }
+
+    @Override
+    public Rider addRider(Rider r) {
+        return riDao.addRider(r);
+    }
+
+    @Override
+    public void addRiderToVehicle(Rider r, Vehicle v) {
+        riDao.addRiderToVehicle(r, v);
+    }
+
+    @Override
+    public void addRiderToStation(Rider r, Station s) {
+        riDao.addRiderToStation(r, s);
+    }
+
+    @Override
+    public void updateRider(Rider r) {
+        riDao.updateRider(r);
+    }
+
+    @Override
+    public void deleteRider(Rider r) {
+        riDao.deleteRider(r);
+    }
     
 }
